af68528fcc938743b2b01526edd5172743fd29ef
Emily Drew L <DontDenyHighFives@gmail.com>
2024-09-03 09:30:03 -0500

    urls changed

--- vocalfryapi/views/__init__.py
+++ vocalfryapi/views/__init__.py
@@ -1,4 +1,4 @@
-from .user_views import UserView, UserSerializer
+from .user_views import UserView, UserSerializer, check_user, register_user
 from .profile_views import ProfileView, ProfileSerializer
 from .category_views import CategoryView, CategorySerializer
 from .profile_category_views import ProfileCategoryView, ProfileCategorySerializer
